65589740a707fd673e30dae879e714d61ae12eaa
tde-nico <tommyden5@gmail.com>
2025-03-15 22:06:02 +0100

    permutations/

---
+++ intro/permutations/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	if (n == 1) {
+		cout << "1\n";
+		return 0;
+	}
+
+	if (n <= 3) {
+		cout << "NO SOLUTION\n";
+		return 0;
+	}
+
+	for (int i = 2; i <= n; i += 2) {
+		cout << i << " ";
+	}
+	for (int i = 1; i <= n; i += 2) {
+		cout << i << " ";
+	}
+
+	return 0;
+}
