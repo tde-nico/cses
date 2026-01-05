0622cab1f2e43704fba109636c9b11d5ad7c3829
tde-nico <tommyden5@gmail.com>
2026-01-05 23:56:38 +0100

    intro/mex_grid_construction

---
+++ intro/mex_grid_construction/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	ll n;
+	cin >> n;
+
+	for (ll i = 0; i < n; ++i) {
+		for (ll j = 0; j < n; ++j) {
+			cout << (i ^ j) << " ";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
